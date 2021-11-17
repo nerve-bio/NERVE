eee9c6fe5807fbed59eef1f8eb5ee11e6b3400a7
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-11-17 12:01:27 +0100

    Update Protein.py

--- Protein.py
+++ Protein.py
@@ -12,6 +12,7 @@ class Protein:
 		self.tmhmm_seq = None
 		self.list_of_shared_human_peps = [] # in a dictionary with match, query and starting position (start_pos)
 		self.list_of_shared_mouse_peps = []
+		self.list_of_shared_conserv_proteome_peps = []
 		self.list_of_peptides_from_comparison_with_mhcpep_sapiens = [] # here a list of mhcpep match
 		self.list_of_peptides_from_comparison_with_mhcpep_mouse = [] # here a list of mhcpep match
 		self.razor_loops = []
