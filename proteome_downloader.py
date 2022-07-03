aa70ae8782ce87c876212c6ffcffeaafa2a5e46d
FranceCosta <francesco.costa.11@studenti.unipd.it>
2022-07-03 15:42:41 +0200

    Update uniprot api

--- proteome_downloader.py
+++ proteome_downloader.py
@@ -42,27 +42,29 @@ def main():
     args = get_args()
     proteome_downloader(args.proteome_id, args.out_filename, args.output_dir)
 
-def proteome_downloader(proteome_id, filename='input_proteome.fasta', output_dir=os.getcwd(), format = "fasta") -> None:
-  """Downloads proteome from uniprot database into output multifasta file
-  param: proteome_id: uniprot unique proteome id, not case-sensitive
-  param: output_dir: output directory (default: current directory)
-  param: format: uniprot API required format (default:fasta)
-  param: filename: output proteome filename (default: input_proteome.fasta)
-  """
-  url = f'https://www.uniprot.org/uniprot/?query=proteome:{proteome_id}&format={format}' 
-  #filename = filename
-  #output_dir = output_dir if output_dir != None else os.getcwd()
-  response = requests.get(url, stream = True)
-  text_file = open(os.path.join(output_dir, filename), 'wb')
-  for chunk in response.iter_content(chunk_size=1024):
-        text_file.write(chunk)
-  # raise an AssertionError if the given proteome ID is not valid
-  assert text_file.tell() > 0, f'{proteome_id} is not a valid Uniprot id'
-  text_file.close()
-  #print(f'Proteome {proteome_id} downloaded succesfully')
-  #output = open(os.path.join(output_dir, filename), 'r').readlines()
-  #return output
-  return None
+def proteome_downloader(proteome_id, filename='input_proteome.fasta', output_dir=os.getcwd(), format_ = "fasta") -> None:
+    """Downloads proteome from uniprot database into output multifasta file
+    param: proteome_id: uniprot unique proteome id, not case-sensitive
+    param: output_dir: output directory (default: current directory)
+    param: format: uniprot API required format (default:fasta)
+    param: filename: output proteome filename (default: input_proteome.fasta)
+    """
+    # use updated uniprot api:
+    url=f'https://rest.uniprot.org/uniprotkb/stream?format={format_}&query=%28proteome%3A{proteome}%29'
+    #url = f'https://www.uniprot.org/uniprot/?query=proteome:{proteome_id}&format={format}' 
+    #filename = filename
+    #output_dir = output_dir if output_dir != None else os.getcwd()
+    response = requests.get(url, stream = True)
+    text_file = open(os.path.join(output_dir, filename), 'wb')
+    for chunk in response.iter_content(chunk_size=1024):
+          text_file.write(chunk)
+    # raise an AssertionError if the given proteome ID is not valid
+    assert text_file.tell() > 0, f'{proteome_id} is not a valid Uniprot id'
+    text_file.close()
+    #print(f'Proteome {proteome_id} downloaded succesfully')
+    #output = open(os.path.join(output_dir, filename), 'r').readlines()
+    #return output
+    return None
     
 if __name__ == "__main__":
     main()
