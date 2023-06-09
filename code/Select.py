0196fd169052ac09fde736d96313b21c2680257c
Francesco Patane, MSc <francesco.patane@live.it>
2023-06-09 15:57:26 +0200

    Update Select.py

--- code/Select.py
+++ code/Select.py
@@ -90,7 +90,8 @@ def output(list_of_proteins:list, outfile, mouse_peptides_sum_limit:float, mouse
                  str(", ".join(list(set(protein.list_of_peptides_from_comparison_with_mhcpep_mouse)))),  
                  str(protein.sequence),
                  str("".join([str(protein.original_sequence_if_razor) if protein.original_sequence_if_razor!=None else ""])),
-                 str("".join([str(protein.tmhmm_seq) if "M" in str(protein.tmhmm_seq) else ""])) # should be shown anyways
+                 str("".join([str(protein.tmhmm_seq) if "M" in str(protein.tmhmm_seq) else ""])), # should be shown anyways
+                 
                  ] for protein in list_of_proteins
                 ], 
                 columns= ['id',
@@ -114,6 +115,10 @@ def output(list_of_proteins:list, outfile, mouse_peptides_sum_limit:float, mouse
                     'sequence',
                     'original_sequence_if_razor',
                     'tmhmm_seq',
+                    'MHC1_epitope',
+                    'MHC2_epitope',
+                    'MHC1_PB',
+                    'MHC2_PB'
                      ]
                 )
     df = df.sort_values(by = 'score', ascending = False)
