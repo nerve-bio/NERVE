399d0ac515e0c039667d98873c2259bdf74fd4c6
Francesco Patane, MSc <francesco.patane@live.it>
2023-06-26 21:01:42 +0200

    Update Select.py

--- code/Select.py
+++ code/Select.py
@@ -70,7 +70,7 @@ def output(list_of_proteins:list, outfile, mouse_peptides_sum_limit:float, mouse
                  str(protein.transmembrane_doms),
                  str(protein.localization[0].localization),
                  str(protein.localization[0].reliability),
-                 str(", ".join([str(element) for element in protein.localization])),
+                 #str(", ".join([str(element) for element in protein.localization])),
                  str("".join([str(round(protein.p_vir,4)) if protein.p_vir!=None else ""])),
                  str("".join([str(round(protein.p_ad, 4)) if protein.p_ad!=None else ""])),
                  str("".join([str(round(protein.conservation_score, 4)) if protein.conservation_score!=None else ""])),
@@ -78,7 +78,7 @@ def output(list_of_proteins:list, outfile, mouse_peptides_sum_limit:float, mouse
                  str("".join(str(len([str(dic['match']) for dic in protein.list_of_shared_mouse_peps if len(protein.list_of_shared_mouse_peps)>0])))),
                  str("".join(str(len([str(dic['match']) for dic in protein.list_of_shared_conserv_proteome_peps if len(protein.list_of_shared_conserv_proteome_peps)>0])))),
                  str("".join([str(round(protein.sapiens_peptides_sum,4)) if protein.sapiens_peptides_sum!=None else "0"])),
-           
+                 str("".join([str(round(protein.mouse_peptides_sum,4)) if protein.mouse_peptides_sum!=None else "0"])),
                  str("".join([str(protein.annotations) if protein.annotations!=None else ""])),
                  str(", ".join(list(set(protein.list_of_peptides_from_comparison_with_mhcpep_sapiens)))), 
                  str(", ".join(list(set(protein.list_of_peptides_from_comparison_with_mhcpep_mouse)))),  
