8dad28b9b4e04e6882a58458a42b3bcf7b2f8d59
Francesco Patane, MSc <francesco.patane@live.it>
2023-06-26 21:03:54 +0200

    Update Razor.py

--- code/Razor.py
+++ code/Razor.py
@@ -12,11 +12,25 @@ def razor(list_of_proteins, working_dir, transmemb_doms_limit, razlen)->list:
                         force=True)
                         
     #logging.debug("Warning: razor uses X as an exclusive symbol to split the final protein. Check if X is used inside the protein sequence!")
-    for p in list_of_proteins:
-        if p.transmembrane_doms >= transmemb_doms_limit:
-            longest_loop = max(p.provide_raw_loops(), key = lambda k: len(k))
-            if len(longest_loop) > razlen:
-                logging.debug(f"Substituting {str(p.id)} sequence with its longest loop.")
-                p.original_sequence_if_razor = p.sequence
-                p.sequence = longest_loop
+    for protein in list_of_proteins:
+        if transmem_doms_limit = 0:
+            if protein.transmembrane_doms >= transmem_doms_limit:
+            
+                new_loop = protein.provide_raw_loops(transmem_doms_limit)
+        else
+            if transmem_doms_limit > 0:
+                if protein.transmembrane_doms >= transmem_doms_limit:
+                    new_loop = max(p.provide_raw_loops_std(), key = lambda k: len(k))
+            
+            
+            
+            if len(new_loop) > min_loop_length:
+                logging.debug(f'Substituting {str(protein.id)} sequence with its outer loops')
+                protein.original_sequence_if_razor = protein.sequence
+                protein.sequence = new_loop
+                protein.razored = True
+            else:
+                logging.debug(f"No replacement found for {str(protein.id)}")
+                protein.razored = False
+        
     return list_of_proteins
