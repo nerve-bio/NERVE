a4300a6496bd5821e667e305a88e27898d944d3a
Francesco Patane, MSc <francesco.patane@live.it>
2023-08-12 09:40:49 +0200

    Update Razor.py

--- code/Razor.py
+++ code/Razor.py
@@ -15,7 +15,7 @@ def razor(list_of_proteins, working_dir, transmemb_doms_limit, razlen)->list:
    
                         
     for protein in list_of_proteins:
-        if transmemb_doms_limit > 2:
+        if transmemb_doms_limit:
             if protein.transmembrane_doms >= transmemb_doms_limit:
             
                 new_loop = max(protein.provide_raw_loops_std(), key = lambda k: len(k))
