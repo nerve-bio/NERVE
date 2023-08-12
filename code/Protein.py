b7f57f9b28568d8a9ebece95c120450f14466f89
Francesco Patane, MSc <francesco.patane@live.it>
2023-08-12 11:14:52 +0200

    Update Protein.py

--- code/Protein.py
+++ code/Protein.py
@@ -11,6 +11,7 @@ class Protein:
 		self.accession = identifier.split('|')[1] if identifier.count("|")==2 else None
 		self.sequence = sequence_string # the sequence used for the analyses
 		self.original_sequence_if_razor = None # put the original sequence if razor is performed
+		self.sequence_out = None
 		self.length = len(sequence_string)
 		self.localization = None
 		self.p_ad = None
@@ -186,6 +187,7 @@ class Protein:
                  str(", ".join(list(set(protein.list_of_peptides_from_comparison_with_mhcpep_mouse)))),  
                  str(protein.sequence),
                  str("".join([str(protein.original_sequence_if_razor) if protein.original_sequence_if_razor!=None else ""])),
+	         str("".join([str(protein.sequence_out) if protein.sequence_out!=None else ""])),
                  str("".join([str(protein.tmhmm_seq) if "M" in str(protein.tmhmm_seq) else ""])),
 		 str("".join([str(protein.MHC1_binders) if protein.MHC1_binders != None else ""])),
                  str("".join([str(protein.MHC2_binders) if protein.MHC2_binders != None else ""])),
@@ -214,8 +216,9 @@ class Protein:
                     'list_of_peptides_from_comparison_with_mhcpep_mouse',
                     'sequence',
                     'original_sequence_if_razor',
+		    'sequence_out',
                     'tmhmm_seq'
-		    		'MHC1_binders',
+		    'MHC1_binders',
                     'MHC2_binders',
                     'MHC1_pb_binders',
                     'MHC2_pb_binders',
