96859f7f43cf1823c90d54e952cf3402ae0fa37f
FranceCosta <franci10000@gmail.com>
2023-08-31 08:04:27 +0100

    Epitope bug fix

--- NERVE.sh
+++ NERVE.sh
@@ -11,7 +11,7 @@
 [ ! "$(docker ps | grep francecosta/psortb_http_api:v0.0.1)" ] &&  docker run --rm -p 8080:8080 --network nerve-network --name psortb -d francecosta/psortb_http_api:v0.0.1
 
 # build nerve if not already built
-[ ! "$(docker images -q nerve:v0.0.2)" ] && docker build -t nerve:v0.0.2 .
+[ ! "$(docker images -q nerve:v0.0.3)" ] && docker build -t nerve:v0.0.3 .
 
 # run nerve container
-docker run --network nerve-network -p 8880:8880 -it -v $(pwd)/:/workdir nerve:v0.0.2 "$@"
+docker run --network nerve-network -p 8880:8880 -it -v $(pwd)/:/workdir nerve:v0.0.3 "$@"
