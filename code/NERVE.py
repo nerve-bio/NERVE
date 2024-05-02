59b9ff2f6059cfd3f642f767eee37ef8652bb52e
Francesco Costa <franci10000@gmail.com>
2024-05-01 22:05:14 -0400

    Update NERVE.py
    Remove p_ad_extracellular_filter and p_ad_no_citoplasm_filter arguments and minor typo corrections

--- code/NERVE.py
+++ code/NERVE.py
@@ -30,8 +30,6 @@ class Args(NamedTuple):
     mouse_peptides_sum_limit:float
     proteome1:str
     proteome2:str
-    p_ad_extracellular_filter:float
-    p_ad_no_citoplasm_filter:float
     padlimit:float
     razor:bool
     razlen:int
@@ -56,8 +54,7 @@ class Args(NamedTuple):
         return (f'''annotation: {self.annotation}, e_value: {self.e_value}, gram: {self.gram},
                 minlength: {self.minlength}, mismatch: {self.mismatch}, mouse: {self.mouse},
                 mouse_peptides_sum_limit: {self.mouse_peptides_sum_limit}, proteome1: {self.proteome1},
-                proteome2: {self.proteome2}, p_ad_extracellular_filter: {self.p_ad_extracellular_filter},
-                padlimit: {self.padlimit}, razor: {self.razor}, razlen: {self.razlen}, select: {self.select},
+                proteome2: {self.proteome2}, padlimit: {self.padlimit}, razor: {self.razor}, razlen: {self.razlen}, select: {self.select},
                 substitution: {self.substitution}, transmemb_doms_limit: {self.transmemb_doms_limit},
                 virlimit: {self.virlimit}, virulent: {self.virulent},epitopes: {self.epitopes}, mhci_length: {self.mhci_length},
                 mhcii_length: {self.mhcii_length}, mhci_overlap: {self.mhci_overlap}, mhcii_overlap: {self.mhcii_overlap},
@@ -129,21 +126,7 @@ def get_args() -> Args:
                         help='Path to proteome or Uniprot proteome ID (see: https://www.uniprot.org/proteomes/?query=&sort=score)',
                         type=str,
                         required=False,
-                        )
-    parser.add_argument('-paefilter','--p_ad_extracellular_filter',
-                        metavar='\b', 
-                        help="Parameter of select module. Extracellular proteins with a probability of adhesin (pad) lower than p_ad_extracellular_filter are discarded (0.-1)",
-                        type=float,
-                        default=0.38,
-                        required=False,
-                        )
-    parser.add_argument('-pacfilter','--p_ad_no_citoplasm_filter',
-                        metavar='\b', 
-                        help="Parameter of select module. Non-cytoplasmic Proteins with a probability of adhesin (pad) lower than p_ad_no_citoplasm_filter are discarded (0.-1)",
-                        type=float,
-                        default=0.46,
-                        required=False,
-                        )    
+                        )   
     parser.add_argument('-pl','--padlimit',
                         metavar='\b',
                         help="Set the probability of adhesin (pad) value cut-off for proteins with 'Unknown' localization in the select module. Thus, these proteins with a pad value < cut-off are discarded (0.-1)",
@@ -203,7 +186,7 @@ def get_args() -> Args:
     parser.add_argument('-ep', '--epitopes',
                         metavar='\b',
                         type=str,
-                        help='Activate or deactivate epitopes module',
+                        help='Activate or deactivate epitope prediction module',
                         required=False,
                         default="True"
                         )
@@ -242,7 +225,7 @@ def get_args() -> Args:
     parser.add_argument('-prt', '--epitope_percentile',
                         metavar='\b',
                         type=float,
-                        help='percentile decision threshold on whick to predict epitopes from full length proteins',
+                        help='percentile decision threshold on which to predict epitopes from full length proteins',
                         required=False,
                         default=0.9
                         )
@@ -288,7 +271,7 @@ def get_args() -> Args:
 
     return Args(args.annotation, args.e_value, args.gram, args.minlength, args.mismatch,
                 args.mouse, args.mouse_peptides_sum_limit, args.proteome1, args.proteome2, 
-                args.p_ad_extracellular_filter, args.p_ad_no_citoplasm_filter, args.padlimit, args.razor, 
+                args.padlimit, args.razor, 
                 args.razlen, args.select, args.substitution, args.transmemb_doms_limit, args.virlimit, 
                 args.virulent, args.epitopes,
                 args.mhci_length, args.mhcii_length, args.mhci_overlap, args.mhcii_overlap,
